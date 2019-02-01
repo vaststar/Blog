68a9d03e1aac9fc5e99aaf0ee0e355958f9a4569
thomas <47029316@qq.com>
2019-02-01 18:04:37 +0800

    reducer

--- Client/blog-web/src/Redux/Reducer/Reducer.js
+++ Client/blog-web/src/Redux/Reducer/Reducer.js
@@ -2,7 +2,5 @@ import { combineReducers } from 'redux'
 
 import {userReducer} from '../ActionReducer/user'
 
-
-
 const CombineReducer = combineReducers({userReducer})
 export default CombineReducer
