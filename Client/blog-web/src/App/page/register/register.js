cc64d8a93e7d672385a48090419a6ddf921d81f6
thomas <47029316@qq.com>
2019-02-11 23:08:50 +0800

    test data

--- Client/blog-web/src/App/page/register/register.js
+++ Client/blog-web/src/App/page/register/register.js
@@ -3,5 +3,12 @@ import {connect} from 'react-redux'
 import { withRouter ,Redirect} from 'react-router-dom'
 
 class Register extends Component {
-    
+    render() {
+        return (
+            <div>register</div>
+        );
+    }
 }
+
+const  RegisterPage =  withRouter(Register)
+export default RegisterPage
--- Client/blog-web/src/App/routerMap.js
+++ Client/blog-web/src/App/routerMap.js
@@ -1,10 +1,12 @@
 import Login from './page/login/login'
 import HomePage from './page/home/home'
 import VideoPage from './page/home/video'
+import RegisterPage from './page/register/register'
 
 export default [
     { path: "/", name: "App", component: HomePage ,auth:false},
     { path: "/login/", name: "Login", component: Login },
-    { path: "/video/", name: "Vedio", component: VideoPage ,auth:true}
+    { path: "/video/", name: "Vedio", component: VideoPage ,auth:true},
+    { path: "/register/", name: "Register", component: RegisterPage ,auth:false}
 ]
 
