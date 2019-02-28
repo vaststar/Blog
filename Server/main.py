dc697b4da9ad42544c38074db468a4db2191bb44
thomas <47029316@qq.com>
2019-02-28 16:34:01 +0800

    canvas

--- Server/main.py
+++ Server/main.py
@@ -5,12 +5,14 @@ sys.path.append('..')
 from Server.ServerApp import ServerApp
 from Server.ServerView import BLUEPRINT
 
+# 注册蓝图
+for k, v in BLUEPRINT.items():
+    ServerApp.registerBluePrint(v, k)
+
 app = ServerApp.app
+
 if __name__ == "__main__":
 
-    #注册蓝图
-    for k,v in BLUEPRINT.items():
-        ServerApp.registerBluePrint(v,k)
     #运行app
     # app.RunApp('0.0.0.0',4444,debug=True)
     #运行manager
