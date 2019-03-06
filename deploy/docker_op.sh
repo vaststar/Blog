a2d9b9a20af53c97874fb3bd8205afa45f0f68bd
thomas <47029316@qq.com>
2019-03-06 17:59:20 +0800

    op

---
+++ deploy/docker_op.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum install -y
+yum remove docker docker-common docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+systemctl start docker
+systemctl enable docker
+curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose up -d
