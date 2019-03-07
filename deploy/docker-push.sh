b5fc0638d49de0b43dbb5a0932b011004a85ecf9
thomas <47029316@qq.com>
2019-03-07 16:01:50 +0800

    deploy

--- deploy/docker-push.sh
+++ deploy/docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker login --username=正天大学士 registry.cn-hangzhou.aliyuncs.com
+sudo docker login --username=正天大学士 --password=ZZT06118115 registry.cn-hangzhou.aliyuncs.com
 sudo docker pull registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
 sudo docker tag a8ec9b03e9df registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
 sudo docker push registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
