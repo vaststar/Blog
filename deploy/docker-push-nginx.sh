b1af3f63991e5da4e045ca72bac1ced46f535afa
vaststar <47029316@qq.com>
2019-03-07 02:16:31 -0500

    de

---
+++ deploy/docker-push-nginx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo docker tag 5b9040fed4cc registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
+sudo docker push registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
