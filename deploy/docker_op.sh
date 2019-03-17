7e4cf70279378b0e56568d9d253dd67b9be795ec
thomas <47029316@qq.com>
2019-03-17 14:11:44 +0800

    op

--- deploy/docker_op.sh
+++ deploy/docker_op.sh
@@ -6,9 +6,9 @@ yum remove docker docker-common docker-engine
 yum install -y yum-utils device-mapper-pesistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce
-systemctl stat docker
+systemctl start docker
 systemctl enable docker
-cul -L https://github.com/docker/compose/releases/download/1.24.0-c1/docker-compose-`uname -s`-`uname -m` -o /us/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.0-c1/docker-compose-`uname -s`-`uname -m` -o /us/local/bin/docker-compose
 chmod +x /us/local/bin/docker-compose
 chmod +x docker-pull.sh
 ./docker-pull.sh
