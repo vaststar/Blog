e5a2f6c13e88c1d41fd1deb3a8a0e1243cc3afa8
thomas <47029316@qq.com>
2019-03-17 14:38:10 +0800

    op

--- deploy/docker_op.sh
+++ deploy/docker_op.sh
@@ -8,8 +8,8 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install -y docker-ce
 systemctl start docker
 systemctl enable docker
-curl -L https://github.com/docker/compose/releases/download/1.24.0-c1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /us/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 chmod +x docker-pull.sh
 ./docker-pull.sh
 docker-compose up -d
