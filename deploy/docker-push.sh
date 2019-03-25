b0b604be707e1444b51efe4dfc1d9fe2f6cc2ea9
thomas <47029316@qq.com>
2019-03-25 23:54:56 +0800

    der

--- deploy/docker-push.sh
+++ deploy/docker-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo docker login --username=正天大学士 --password=ZZT06118115 registry.cn-hangzhou.aliyuncs.com
-sudo docker tag a8ec9b03e9df registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
+sudo docker tag bb541cfb215b registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
 sudo docker push registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
-sudo docker tag 5b9040fed4cc registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
+sudo docker tag 5d938d423756 registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
 sudo docker push registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
+sudo docker tag 00585e6cf4a8 registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_mysql:1.0
+sudo docker push registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_mysql:1.0
+
