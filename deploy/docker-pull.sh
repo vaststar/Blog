2243e146f0f9f37a78773f494d184f381b15d364
thomas <47029316@qq.com>
2019-03-26 09:03:44 +0800

    pull

--- deploy/docker-pull.sh
+++ deploy/docker-pull.sh
@@ -2,5 +2,8 @@
 sudo docker login --username=正天大学士 --password=ZZT06118115 registry.cn-hangzhou.aliyuncs.com
 sudo docker pull registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0
 sudo docker pull registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0
+sudo docker pull registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_mysql:1.0
 sudo docker tag registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_app:1.0 blog/app:latest
 sudo docker tag registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_nginx:1.0 blog/nginx:latest
+sudo docker tag registry.cn-hangzhou.aliyuncs.com/master_thomas/blog_mysql:1.0 blog/mysql:latest
+
