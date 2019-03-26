a67521f4d3001fdae71eb3fa8a23007eb509cf7e
thomas <47029316@qq.com>
2019-03-26 09:38:25 +0800

    sql

---
+++ clean_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f Server/log/*.log
+rm -f Server/log/*.pid
+rm -f nginx/logs/*.log
+rm -rf mysql/data/*
--- mysql/mysql_sql.sql
+++ mysql/mysql_sql.sql
@@ -1,6 +1,6 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
 ALTER USER 'root'@'%' IDENTIFIED BY 'root' PASSWORD EXPIRE NEVER;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
 flush privileges;
 create database if not exists blog;
 use blog;
