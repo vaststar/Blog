d55d5fdff9031a739aaa383f55bf680ea7d67f7d
thomas <47029316@qq.com>
2019-04-11 21:25:35 +0800

    conf

--- Server/app/ServerLog/__init__.py
+++ Server/app/ServerLog/__init__.py
@@ -1,6 +1,8 @@
 import os
 import logging.config
 
+if not os.path.isdir(os.path.join(os.getcwd(),'log')):
+    os.makedirs(os.path.join(os.getcwd(),'log'))
 logging.config.fileConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)),'log.ini'))
 logger=logging.getLogger('server')
 logger.setLevel(logging.DEBUG)
